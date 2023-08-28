c095c33e562c598ddae11f516c6612f9d593f86b
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-08-28 15:43:35 +0600

    Added notes about CRUD operations of API.

---
+++ 6_JavaScriptES6_API/33_JSON_API/crudOperations.js
@@ -0,0 +1,20 @@
+/*
+CRUD: CRUD stands for Create, Read, Update, Delete. These are the four fundamental operations that can be performed on data within a database or any kind of storage system.
+
+Create (C): This operation involves adding new data to the system. For example, creating a new user account, posting a new article, or adding a new product to an online store.
+Read (R): This operation involves retrieving or reading existing data from the system. For example, fetching user profiles, reading articles, or getting information about products.
+Update (U): This operation involves modifying existing data. It allows you to make changes to the data that's already stored. For example, updating a user's profile information or editing the content of an article.
+Delete (D): This operation involves removing data from the system. For example, deleting a user account, removing an article, or taking a product down from a store.
+
+----------------------------------------------------------------
+
+PATCH: PATCH is a specific HTTP verb (or method) used in the context of APIs. It is used to make partial updates to a resource. Instead of sending a full set of data for an update, you only send the changes that need to be made. This can be more efficient when you only want to modify specific fields of a resource without affecting the rest.
+
+DELETE: DELETE is another HTTP verb used in APIs. It's used to request the removal of a resource from the server. When you send a DELETE request, you're indicating that you want to delete the specified resource.
+
+GET: GET is an HTTP verb used to retrieve data from a server. When you send a GET request, you're asking the server to provide you with the data associated with the requested resource. It's a read-only operation and shouldn't cause any changes to the server's data.
+
+PUT: PUT is an HTTP verb used to update or create a resource on the server. It's often used to send a full replacement of the resource. In other words, if you send a PUT request to a specific resource, you're essentially replacing the entire existing resource with the new data you provide.
+
+POST: POST is another HTTP verb used in APIs. It's used to send data to the server to create a new resource. When you send a POST request, you're essentially submitting data to the server, and the server then processes that data and creates a new resource based on the provided information.
+*/
